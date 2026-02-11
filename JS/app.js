f771f2ae4f8168e9ca2789e23f9db8baecd97d83
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-02-11 23:12:05 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1487,14 +1487,14 @@ function checkMissedDeadlines() {
 
                              if(completeMissions < 0){
                         
-                completedMissions -= 4; 
-localStorage.setItem("completedMissions", completedMissions);
-document.getElementById("missionCounter").textContent = completedMissions;  
+                completeMissions -= 4; 
+localStorage.setItem("completeMissions", completeMissions);
+document.getElementById("missionCounter").textContent = completeMissions;  
                 }
                 else{
 
-           completedMissions = 0;
-localStorage.setItem("completedMissions", 0);
+           completeMissions = 0;
+localStorage.setItem("completeMissions", 0);
 document.getElementById("missionCounter").textContent = "0";
 
                 }
@@ -2318,6 +2318,7 @@ function skipDayCheat() {
 
   console.log("⏭ Day skipped to:", nextDayKey);
 };
+
 
 
 
