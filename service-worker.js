cc2a52fe7135f227103fe8b3951938e19dbf5a51
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-02-14 17:39:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "standout-v2.1.36";
+const CACHE_NAME = "standout-v2.1.37";
 //const MEDIA_CACHE = "standout-media";     // NEVER versioned
 
 const APP_SHELL = [
@@ -142,6 +142,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
