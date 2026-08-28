e4c9ef28f2f2e087ce4b2dc0f6017ba8c1e60bf3
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-08-28 12:50:56 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "standout-v2.3 beta 17";
+const CACHE_NAME = "standout-v2.3 beta 18";
 //const MEDIA_CACHE = "standout-media";
 // NEVER versioned
 
@@ -6,7 +6,7 @@ const FONT_AWESOME_CACHE =
   "standout-fontawesome-v1";
 
 const BACKGROUND_CACHE =
-  "standout-background-v5";
+  "standout-background-v6";
 
 const WELCOME_CACHE =
   "standout-welcome-v3";
